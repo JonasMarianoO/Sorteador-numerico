a1ba98ddca7d257b57268f2eecb3d048b4382fbf
JonasMarianoO <111180615+JonasMarianoO@users.noreply.github.com>
2022-12-28 19:37:48 -0300

    1 file modified

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/*const botaoSortear = document.querySelector('div.box button');
-
-var numeroSorteado = document.querySelector('div.box h1');
-var roleta = [];
-var i = 1;
-
-for(i = 1; i <= 60; i++){
-roleta.push(i)
-}
-
-botaoSortear.addEventListener('click', () => {
-numeroSorteado.innerHTML = roleta[Math.floor([Math.random() * roleta.length])];
-});*/
-
 function getRandomNumbers() {
   let numbers = [];
 
